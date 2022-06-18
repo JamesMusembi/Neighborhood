ba98b125d96f574f577ebc30ba24969449240e0d
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-18 21:55:28 +0300

    Add post test

--- hood/tests.py
+++ hood/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import NeighborHood, Profile, Business
+from .models import NeighborHood, Profile, Business, Post
 
 class ProfileTestCase(TestCase):
     def setUp(self):
@@ -23,6 +23,17 @@ class BusinessTestCase(TestCase):
         self.business.save_business()
         business = Business.objects.all()
         self.assertTrue(len(business) > 0)
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="melo",email="melo@gmail.com",password="983di")
+        self.posts = Post(user=user,title="politics",content="politics isa dirty game")
+    def test_instance(self):
+        self.assertTrue(isinstance(self.posts,Post))
+    def save_post_method(self):
+        self.posts.save_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
 
 
 """ class NeighborTestClass(TestCase):
